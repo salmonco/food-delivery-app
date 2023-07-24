c1a89819fc9a5383b69804ed4b2b85cc80c4d233
salmonco <xkqjsslsek80@naver.com>
2023-07-24 18:16:14 +0900

    connect redux

---
+++ AppInner.tsx
@@ -0,0 +1,66 @@
+import SignIn from './src/pages/SignIn';
+import SignUp from './src/pages/SignUp';
+import Orders from './src/pages/Orders';
+import Delivery from './src/pages/Delivery';
+import Settings from './src/pages/Settings';
+import * as React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {useSelector} from 'react-redux';
+import {RootState} from './src/store/reducer';
+
+export type LoggedInParamList = {
+  Orders: undefined;
+  Settings: undefined;
+  Delivery: undefined;
+  Complete: {orderId: string};
+};
+
+export type RootStackParamList = {
+  SignIn: undefined;
+  SignUp: undefined;
+};
+
+const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function AppInner() {
+  // useSelector()는 <Provider> 내부에서만 사용 가능
+  const isLoggedIn = useSelector((state: RootState) => !!state.user.email);
+  console.log('isLoggedIn', isLoggedIn);
+
+  return isLoggedIn ? (
+    <Tab.Navigator>
+      <Tab.Screen
+        name="Orders"
+        component={Orders}
+        options={{title: '오더 목록'}}
+      />
+      <Tab.Screen
+        name="Delivery"
+        component={Delivery}
+        options={{title: '내 오더'}}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={Settings}
+        options={{title: '내 정보'}}
+      />
+    </Tab.Navigator>
+  ) : (
+    <Stack.Navigator>
+      <Stack.Screen
+        name="SignIn"
+        component={SignIn}
+        options={{title: '로그인'}}
+      />
+      <Stack.Screen
+        name="SignUp"
+        component={SignUp}
+        options={{title: '회원가입'}}
+      />
+    </Stack.Navigator>
+  );
+}
+
+export default AppInner;
